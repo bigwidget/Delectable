5ce4e33fb0bde8e9487673d5d93c6157aea3e51f
Tim Koelkebeck <tim@mytype.com>
2011-03-21 19:10:31 -0400

    remove migration file

--- db/migrate/20110321021204_add_first_visit_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddFirstVisitToUser < ActiveRecord::Migration
-  def self.up
-    add_column :users, :first_visit, :boolean, :default => true
-  end
-
-  def self.down
-    remove_column :users, :first_visit
-  end
-end
